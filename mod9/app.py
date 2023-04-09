5b962d9b3b3ff48a6623d1f0e6425a41f5ad71d5
mpirtgod <AVMelnikov@urfu.me>
2023-04-09 19:53:50 +0500

    mod9

---
+++ mod9/app.py
@@ -0,0 +1,27 @@
+import datetime
+import os
+import re
+from flask import Flask, render_template, send_from_directory
+import random
+
+
+root_dir = os.path.dirname(os.path.abspath(__file__))
+template_folder = os.path.join(root_dir, "templates")
+js_dir = os.path.join(template_folder, 'js')
+
+app = Flask(__name__, template_folder=template_folder)
+
+
+
+@app.route('/')
+def hello_world():
+    return render_template("index.html")
+
+
+@app.route("/js/<path:path>")
+def send_js(path):
+    return send_from_directory(js_dir, path)
+
+
+if __name__ == '__main__':
+    app.run()
