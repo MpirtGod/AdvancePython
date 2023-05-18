12d3b7e5ea15578a2b2aab8a44e28db56ae3ed2f
mpirtgod <AVMelnikov@urfu.me>
2023-05-18 23:14:20 +0500

    mod12

--- mod12/task2.py
+++ mod12/task2.py
@@ -1,5 +1,9 @@
+import multiprocessing
+from multiprocessing.pool import ThreadPool, Pool
 import subprocess
+import os
 
+# memory_usage = 0
 
 def process_count(username: str) -> int:
     output = subprocess.check_output(["ps", "-u", username, "-o", "pid="])
@@ -7,15 +11,33 @@ def process_count(username: str) -> int:
 
 
 def total_memory_usage(root_pid: int) -> float:
-    output = subprocess.check_output(["ps", "--ppid", str(root_pid), "-o", "rss="])
-    memory_usage = sum(map(int, output.splitlines()))
-    output = subprocess.check_output(["ps", "-p", str(root_pid), "-o", "rss="])
-    memory_usage += int(output.splitlines()[1])
-    output = subprocess.check_output(["ps", "--ppid", str(root_pid), "-o", "pid="])
-    child_pids = list(map(int, output.splitlines()))
-    for pid in child_pids:
-        output = subprocess.check_output(["ps", "-p", str(pid), "-o", "rss="])
-        memory_usage += int(output.splitlines()[1])
-    output = subprocess.check_output(["free", "-m"])
-    total_memory = int(output.splitlines()[1].split()[1])
-    return (memory_usage / total_memory) * 100
+    # global memory_usage
+    memory_usage = 0
+    pids_command = f"pgrep -P {root_pid}"
+    pids_output = os.popen(pids_command).read()
+    pids = pids_output.strip().split('\n')
+    if pids == ['']:
+        pids = []
+    pids.append(str(root_pid))
+    # with Pool(processes=multiprocessing.cpu_count()) as pool:
+    #     pool.map(get_memory_usage_by_pid, pids)
+    for pid in pids:
+        ps_command = f"ps -p {pid} -o rss="
+        ps_output = os.popen(ps_command).read()
+        if ps_output != '':
+            memory_usage += int(ps_output)
+    output = os.popen("free -k").readlines()
+    total_memory = output[1]
+    total_memory = total_memory.split()[2]
+    total_memory = int(total_memory)
+    return round(memory_usage/total_memory, 2) * 100
+
+
+# def get_memory_usage_by_pid(pid):
+#     global memory_usage
+#     ps_command = f"ps -p {pid} -o rss="
+#     ps_output = os.popen(ps_command).read()
+#     memory_usage += int(ps_output)
+
+
+print(total_memory_usage(3227))
